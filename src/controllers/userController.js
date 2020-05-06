4832b2b15ed76015469e76564d5c19bc43978650
kushal-kamra <kushalkamra@gmail.com>
2020-05-06 10:13:48 +0530

    Correcting JWT package

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
-import jwt from 'jwt';
+import jwt from 'jsonwebtoken';
 import { UserSchema } from '../models/userModel';
 
 const User = mongoose.model('User', UserSchema);
