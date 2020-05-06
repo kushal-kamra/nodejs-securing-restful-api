cd5b7ed98a8c1dae534986a37fba119b7e599932
kushal-kamra <kushalkamra@gmail.com>
2020-05-06 10:14:58 +0530

    Comments and routes for login, registeration

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -6,27 +6,43 @@ import {
     deleteContact 
 } from '../controllers/crmController';
 
+import {
+    login,
+    loginRequired,
+    register
+} from '../controllers/userController';
+
 const routes = (app) => {
     app.route('/contacts')
+
+    // GET all contacts
     .get((req, res, next) => {
         // middleware
         console.log(`Request from: ${req.originalUrl}`)
         console.log(`Request type: ${req.method}`)
         next();
-    }, getContacts)
+    }, loginRequired, getContacts)
     
-    // POST endpoint
-    .post(addNewContact);
+    // POST endpoint - Add new crm contact
+    .post(loginRequired, addNewContact);
 
     app.route('/contact/:contactId')
     // get specific contact
-    .get(getContactWithID)
+    .get(loginRequired, getContactWithID)
     
-    // put request
-    .put(updateContact)
+    // put request - update specific contact
+    .put(loginRequired, updateContact)
 
-    // delete request
-    .delete(deleteContact);
+    // delete request - delete specific contact
+    .delete(loginRequired, deleteContact);
+
+    // registeration route
+    app.route('/auth/register')
+        .post(register);
+
+    // login route
+    app.route('/auth/login')
+        .post(login);
 }
 
 export default routes;
